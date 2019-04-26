9bd0df8a0f6349c9e654263cda8384498a7d81c3
Anshu <anshu.roy@outlook.com>
2019-04-26 13:50:13 +0200

    Additional dimesions - Past two days and seven days implemented

--- SQL/Create_tables/DIM/Create_Resource_Workload.sql
+++ SQL/Create_tables/DIM/Create_Resource_Workload.sql
@@ -1,18 +1,23 @@
-/****** Object:  Table [stg].[event_table]    Script Date: 4/17/2019 3:23:48 PM ******/
+/****** Object:  Table [DIM].[resource_load]    Script Date: 4/26/2019 1:38:37 PM ******/
+DROP TABLE [DIM].[resource_load]
+GO
+
+/****** Object:  Table [DIM].[resource_load]    Script Date: 4/26/2019 1:38:37 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [DIM].[resource_load](
-	
-	[_event_User_] [nvarchar](100) NULL,
-	
-	[_event_time_timestamp_] [datetime] NULL,
+CREATE TABLE [DIM].[RESOURCE_WORKLOAD](
 	[_eventID__] [bigint] NOT NULL,
-	[task_load] [bigint] NOT NULL
- )
+	[_event_User_] [nvarchar](100) NULL,
+	[_event_time_timestamp_] [datetime] NULL,
+	[_case_concept_name_] nvarchar(100) NULL,
+	[_event_concept_name_] nvarchar(100) NULL,
+	[task_load_past_two_days] [bigint] NOT NULL,
+	[task_load_past_seven_days] [bigint] NOT NULL
+) ON [PRIMARY]
 GO
 
 
