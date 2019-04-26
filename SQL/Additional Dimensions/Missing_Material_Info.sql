6d86fef43edcf6c08da23b1ab99d184d00edae3e
Anshu <anshu.roy@outlook.com>
2019-04-26 14:32:12 +0200

    Additional DImensions - Missing Material Info

---
+++ SQL/Additional Dimensions/Missing_Material_Info.sql
@@ -0,0 +1,13 @@
+-- Find all events happened against cases which has no value in the fields, Spend_area_text and sub_spend_area_text
+--indicating missing information about the department and material ordered.
+Insert into [DIM].[MISSING_MATERIAL_INFO]
+select A._eventID__,
+		A._case_concept_name_,
+		A._event_concept_name_,
+		B._case_Spend_area_text_,
+		B._case_Sub_spend_area_text_,
+		B._case_Spend_classification_text_
+from stg.event_table A join stg.case_table B 
+on A._case_concept_name_ = B._case_concept_name_
+where B._case_Spend_area_text_ like
+'' or B._case_Sub_spend_area_text_ like ''
---
+++ SQL/Create_tables/DIM/Create_Missing_Material_Info.sql
@@ -0,0 +1,18 @@
+/****** Object:  Table [DIM].[RESOURCE_WORKLOAD]    Script Date: 4/26/2019 2:21:00 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [DIM].[MISSING_MATERIAL_INFO] (
+	[_eventID__] [bigint] NOT NULL,
+	[_case_concept_name_] [nvarchar](100) NULL,
+	[_event_concept_name_] [nvarchar](100) NULL,
+	[_case_Spend_area_text_] nvarchar(100) NULL,
+    [_case_Sub_spend_area_text_] nvarchar(100) NULL,
+	[_case_Spend_classification_text_] nvarchar(100) NULL
+) ON [PRIMARY]
+GO
+
+
