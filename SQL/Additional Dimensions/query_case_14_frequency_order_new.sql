a120144040474f1c22137303231053fbc25820e4
Vincentxyz <vincentmzw@gmail.com>
2019-05-12 17:26:35 +0200

    Additional_Dimensions

--- SQL/Additional Dimensions/query_case_14_frequency_order_new.sql
+++ SQL/Additional Dimensions/query_case_14_frequency_order_new.sql
@@ -2,12 +2,16 @@
 Calculate frequency of oders by the group(_case_Sub_spend_area_text_,_case_Vendor_,case_Document_Type_) within 1 month
 */
 
-SELECT A._case_Sub_spend_area_text_,
+DROP TABLE IF EXISTS [DIM].[frequency_order_new];
+Go
+
+SELECT A._case_concept_name_
+,A._case_Sub_spend_area_text_,
 		A._case_Vendor_,
 		A._case_Document_Type_,
 	   COUNT(DISTINCT AA._case_concept_name_) AS [FREQ_ORDER]
 
-INTO [DIM].[frequency_order_new]
+INTO [DIM].[case_frequency_order_new]
 
 FROM [PROM].[Event_Log_All] A, [PROM].[Event_Log_All] AA
 		where  A._case_Sub_spend_area_text_=AA._case_Sub_spend_area_text_
@@ -16,4 +20,4 @@ FROM [PROM].[Event_Log_All] A, [PROM].[Event_Log_All] AA
 		AND A._case_Document_Type_ = AA._case_Document_Type_
 		and A._event_time_timestamp_ between DATEADD(dd,-30,AA._event_time_timestamp_) and AA._event_time_timestamp_
 GROUP BY 
-	   A._case_Sub_spend_area_text_,A._case_Vendor_,A._case_Document_Type_
+	   A._case_concept_name_, A._case_Sub_spend_area_text_,A._case_Vendor_,A._case_Document_Type_
