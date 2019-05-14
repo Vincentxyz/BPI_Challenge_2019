a5ab821aff5e55ee2d55d4ae0600428aedc0ccea
Anshu <anshu.roy@outlook.com>
2019-05-14 16:01:01 +0200

    SQL scripts for complaince - add column and update scripts

---
+++ SQL/Additional Dimensions/alter_case_consolidated_add_case_compliance.sql
@@ -0,0 +1,9 @@
+alter table DIM.case_consolidated_dimensions
+add SUM_GR int,
+SUM_IR int,
+Deviation int,
+CreateOrder_NetVal float,
+GR_NetVal float,
+IR_NetVal float,
+CancelGR_NetVal float,
+CancelIR_NetVal float
